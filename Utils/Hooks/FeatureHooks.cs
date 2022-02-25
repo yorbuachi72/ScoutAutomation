53f8d9dc904775d1e1f40cb822520d3a331722df
yorbuachi72 <yorbuachi@gmail.com>
2022-02-25 00:35:19 -0500

    Add project files.

---
+++ Utils/Hooks/FeatureHooks.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoutAutomation.Utils.Hooks
+{
+    internal class FeatureHooks
+    {
+    }
+}
